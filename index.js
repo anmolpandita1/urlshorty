90765f2020348b38b2f50a5fc0f2e529578dbe50
Anmol Pandita <39385082+anmolpandita1@users.noreply.github.com>
2022-06-25 00:35:52 +0530

    init

---
+++ index.js
@@ -0,0 +1,102 @@
+import path from 'path';
+import express  from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import yup from 'yup';
+import monk from 'monk';
+import { rateLimit } from 'express-rate-limit';
+import slowDown from 'express-slow-down';
+import { nanoid } from 'nanoid';
+import  dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
+
+const app = express();
+app.enable('trust proxy');
+
+app.use(helmet());
+app.use(morgan('common'));
+app.use(express.json());
+app.use(express.static('./public'));
+
+const notFoundPath = path.join(__dirname, 'public/404.html');
+
+app.get('/:id', async (req, res, next) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      return res.redirect(url.url);
+    }
+    return res.status(404).sendFile(notFoundPath);
+  } catch (error) {
+    return res.status(404).sendFile(notFoundPath);
+  }
+});
+
+const schema = yup.object().shape({
+  slug: yup.string().trim().matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post('/url', slowDown({
+  windowMs: 30 * 1000,
+  delayAfter: 1,
+  delayMs: 500,
+}), rateLimit({
+  windowMs: 30 * 1000,
+  max: 1,
+}), async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (url.includes('cdg.sh')) {
+      throw new Error('Stop it. 🛑');
+    }
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error('Slug in use. 🍔');
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(notFoundPath);
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
